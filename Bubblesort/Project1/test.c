5ecf283c92421607406a963397450035fb6ccfdc
zhc_code <406680112@qq.com>
2019-11-22 22:41:10 +0800

    冒泡排序

---
+++ Bubblesort/Project1/test.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrintArray(int arr[], int n) {
+	int i = 0;
+	for (; i < n; i++) {
+		printf("%d", arr[i]);
+	}
+	putchar('\n');
+}
+
+void Swap(int* x, int* y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+void Bubblesort(int* arr, int n) {
+
+	int i = 0;
+	int j = 0;
+	int temp = 0;
+	for (i = 0; i < n - 1; i++) {
+		for (j = 0; j < n - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				Swap(&arr[j], &arr[j + 1]);
+			}
+		}
+	}
+	PrintArray(arr,n);
+}
+
+int main() {
+
+	int arr[] = { 1,9,2,7,4 };
+	Bubblesort(arr, 5);
+
+	system("pause");
+	return 0;
+}
