bba29f570f1971dff763110e61f203e99029adc6
zhc_code <406680112@qq.com>
2019-11-25 14:27:41 +0800

    冒泡排序

--- Bubblesort/Project1/test.c
+++ Bubblesort/Project1/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void PrintArray(int arr[], int n) {
+void PrintArray(int arr[], int n) {		//打印数组函数
 	int i = 0;
 	for (; i < n; i++) {
 		printf("%d", arr[i]);
@@ -9,17 +9,17 @@ void PrintArray(int arr[], int n) {
 	putchar('\n');
 }
 
-void Swap(int* x, int* y) {
+void Swap(int* x, int* y) {				//交换两个数字的函数
 	int temp = *x;
 	*x = *y;
 	*y = temp;
 }
-void Bubblesort(int* arr, int n) {
+void Bubblesort(int* arr, int n) {		//冒泡排序函数
 
 	int i = 0;
 	int j = 0;
 	int temp = 0;
-	for (i = 0; i < n - 1; i++) {
+	for (i = 0; i < n - 1; i++) {		
 		for (j = 0; j < n - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
 				Swap(&arr[j], &arr[j + 1]);
