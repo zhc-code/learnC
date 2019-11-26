8a9f43b3483ebac67bbd7d5e4ba9c4edf839f76f
zhc_code <406680112@qq.com>
2019-11-26 22:24:04 +0800

    递归

---
+++ Homework8_Recursive/Homework8_Recursive/test1.c
@@ -0,0 +1,36 @@
+////编写一个函数实现n^k，使用递归实现
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int Function(int n, int k) {
+//
+//	if (k == 0) {
+//		return 1;
+//	}
+//	else if (k == 1) {
+//		return n;
+//	}
+//	else {
+//		return n * Function(n, k - 1);
+//	}
+//}
+//int main() {
+//
+//	int n = 0;
+//	int k = 0;
+//	printf("请输入n^k中的n:\n");
+//	scanf("%d", &n);
+//	printf("请输入n^k中的k:\n");
+//	scanf("%d", &k);
+//	if (k < 0) {					//判断用户输入是否合法
+//		printf("您的输入有误!\n");
+//	}
+//	else {
+//		printf("%d\n", Function(n, k));
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ Homework8_Recursive/Homework8_Recursive/test6.c
@@ -0,0 +1,31 @@
+////递归方式实现打印一个整数的每一位
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//void Print(n) {
+//	if (n < 10) {
+//		printf("%d ", n);
+//	}
+//	else {
+//		Print(n / 10);
+//		printf("%d ", n % 10);
+//	}
+//}
+//
+//int main() {
+//
+//	printf("请输入一个整数:\n");
+//	int n = 0;
+//	scanf("%d", &n);
+//	if (n < 0) {
+//		printf("您的输入有误!\n");
+//	}
+//	else {
+//		Print(n);
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
